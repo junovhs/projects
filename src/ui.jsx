c07894329639c96f43185d95768b4db0e0248f48
junovhs <junovhs@gmail.com>
2025-08-15 14:04:27 -0700

    chore: add ui.jsx re-exports; point App.jsx at them

---
+++ src/ui.jsx
@@ -0,0 +1,3 @@
+export { default as Sidebar } from './components/Sidebar.jsx';
+export { default as ProjectPage } from './components/ProjectPage.jsx';
+export { default as WelcomePage } from './components/WelcomePage.jsx';
